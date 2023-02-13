5089a4aa6a010f6f6745a75e7f0852ac3fd68af2
Jiří Kukuczka <jk.satos@gmail.com>
2023-02-13 09:08:14 +0100

    Continue solving an issue with filtering and displaing

--- Project_REPORT_v7/App_Start/BundleConfig.cs
+++ Project_REPORT_v7/App_Start/BundleConfig.cs
@@ -10,6 +10,7 @@ namespace Project_REPORT_v7
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
                         "~/Scripts/jquery-ui-{version}.js",
                         "~/Scripts/jquery.validate*",
                         "~/Scripts/jquery.unobtrusive-ajax.js",
@@ -19,9 +20,6 @@ namespace Project_REPORT_v7
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
