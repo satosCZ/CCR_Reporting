ba96bebb736d0cd17fad0c70d6079474f60557fe
Jiří Kukuczka <jk.satos@gmail.com>
2023-02-23 05:39:57 +0100

    Started adding custon Error page

---
+++ Project_REPORT_v7/Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_REPORT_v7.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        [HandleError]
+        public ActionResult Error404(int statusCode, Exception exception)
+        {
+            //Response.StatusCode = 404;
+            //Exception ex = new Exception();
+            //ex = Server.GetLastError().GetBaseException();
+            //TempData["Message"] = ex.Message;
+            //return View("Error_404");
+            Response.StatusCode = statusCode;
+            TempData["Status"] = statusCode + " Error";
+            return View();
+        }
+
+        [HandleError]
+        public ActionResult Error500()
+        {
+            return View();
+        }
+
+
+    }
+}
--- Project_REPORT_v7/Global.asax.cs
+++ Project_REPORT_v7/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_REPORT_v7.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,31 @@ namespace Project_REPORT_v7
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void Application_Error(Object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Server.ClearError();
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "Error404");
+            routeData.Values.Add("exception", exception);
+
+            if (exception.GetType() == typeof(HttpException))
+            {
+                routeData.Values.Add("statusCode", ((HttpException)exception).GetHttpCode());
+            }
+            else
+            {
+                routeData.Values.Add("statusCode", 500);
+            }
+
+            Response.TrySkipIisCustomErrors = true;
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+            Response.End();
         }
     }
 }
