d022e2d1486fdb8ac970f029c488b514c4f30981
satosCZ <jk.satos@gmail.com>
2023-05-23 08:59:50 +0200

    Added JS converter for type to browser console

---
+++ Project_REPORT_v7/Controllers/Addon/JSConsoleLog.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+
+namespace Project_REPORT_v7.Controllers.Addon
+{
+    public static class JSConsoleLog
+    {
+        static string scriptTag = "<script type=\"\" language=\"\">{0}</script>";
+
+        public static void ConsoleLog(string message)
+        {
+            string function = "console.log('{0}');";
+            string log = string.Format(GenerateCodeFromFunction(function), message);
+            HttpContext.Current.Response.Write(log);
+        }
+
+        public static void Alert(string message)
+        {
+            string function = "alert('{0}');";
+            string alert = string.Format(GenerateCodeFromFunction(function), message);
+            HttpContext.Current.Response.Write(alert);
+        }
+
+        private static string GenerateCodeFromFunction(string function)
+        {
+            return string.Format(scriptTag, function);
+        }
+    }
+}
