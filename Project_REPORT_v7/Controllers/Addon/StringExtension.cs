6b16d56c8d7efa3d013ead9022e8969133f08077
satosCZ <jk.satos@gmail.com>
2023-06-02 12:51:52 +0200

    Fixed some issue with converting

--- Project_REPORT_v7/Controllers/Addon/StringExtension.cs
+++ Project_REPORT_v7/Controllers/Addon/StringExtension.cs
@@ -44,9 +44,8 @@ namespace Project_REPORT_v7.Controllers.Addon
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
-        public static string ToCapitalize( this string str, bool ending = true )
+        public static string ToCapitalize( this string str )
         {
-            var last = str.Split(' ').Last();
             var temp = str.Split(' ');
             string result = "";
 
@@ -54,41 +53,18 @@ namespace Project_REPORT_v7.Controllers.Addon
             {
                 foreach ( var item in temp )
                 {
-                    if ( !ending )
-                    {
-                        if ( item.StartsWith( " " ) && !item.Equals( last ) )
-                        {
-                            result += item.Substring( 0, 2 ).ToUpper() + item.Substring( 2 ).ToLower() + ". ";
-                        }
-                        else if ( item.StartsWith( " " ) && item.Equals( last ) )
-                        {
-                            result += item.Substring( 0, 2 ).ToUpper() + item.Substring( 2 ).ToLower();
-                        }
-                        else if ( !item.StartsWith( " " ) && item.Equals( last ) )
-                        {
-                            result += item.Substring( 0, 1 ).ToUpper() + item.Substring( 1 ).ToLower();
-                        }
-                        else
-                        {
-                            result += item.Substring( 0, 1 ).ToUpper() + item.Substring( 1 ).ToLower() + ".";
-                        }
-                    }
-                    else
+                    if ( item.Length > 1 )
                     {
                         if ( item.StartsWith( " " ) )
                         {
-                            result += item.Substring( 0, 2 ).ToUpper() + item.Substring( 2 ).ToLower() + ". ";
+                            result += item.Substring( 0, 2 ).ToUpper() + item.Substring( 2 ).ToLower() + " ";
                         }
                         else
                         {
-                            result += item.Substring( 0, 1 ).ToUpper() + item.Substring( 1 ).ToLower() + ".";
+                            result += item.Substring( 0, 1 ).ToUpper() + item.Substring( 1 ).ToLower() + " ";
                         }
                     }
                 }
-            }
-            else
-            {
-                result = temp.Length > 0 ? temp [0].Substring( 0, 1 ).ToUpper() + temp [0].Substring( 1 ).ToLower() : "";
             }
             return result;
         }
