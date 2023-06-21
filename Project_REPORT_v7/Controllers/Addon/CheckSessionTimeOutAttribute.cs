667ab1d4e5f204c5bfd2cb8e79f475caf26361e7
satosCZ <jk.satos@gmail.com>
2023-06-21 11:35:57 +0200

    Added to test if after logon to returnUrl will access current user

--- Project_REPORT_v7/Controllers/Addon/CheckSessionTimeOutAttribute.cs
+++ Project_REPORT_v7/Controllers/Addon/CheckSessionTimeOutAttribute.cs
@@ -24,6 +24,41 @@ namespace Project_REPORT_v7.Controllers.Addon
                         {
                             redirectTo = string.Format("~/Home/Login?returnUrl=[0]", HttpUtility.UrlEncode(context.Request.RawUrl));
                         }
+                        MembersTablesController member = new MembersTablesController();
+                        ADHelper ad = new ADHelper(context.User.Identity.Name);
+                        if ( LDAPHelper.UserIsMemberOfGroups( context.User.Identity.Name, new string [] { "CCR_Report_Admin" } ) )
+                        {
+                            context.Session ["isAdmin"] = "Admin";
+                            context.Session ["Closed"] = "false";
+                            JSConsoleLog.ConsoleLog( $"Loged in as {ad.MemberName} [{ad.MemberID}]" );
+                        }
+                        else
+                        {
+                            context.Session ["isAdmin"] = "NonAdmin";
+                            JSConsoleLog.ConsoleLog( $"Loged in as {ad.MemberName} [{ad.MemberID}]" );
+                        }
+
+                        if ( !member.CheckMember( ad.MemberID ) )
+                        {
+                            JSConsoleLog.ConsoleLog( $"User ID {ad.MemberID} & User Name {ad.MemberName} is not in local DB" );
+                            if ( member.AddMember( ad ) )
+                            {
+                                context.Session ["User"] = $"{ad.MemberName} [{ad.MemberID}]";
+                                context.Session ["UserID"] = ad.MemberID;
+                            }
+                            else
+                            {
+                                JSConsoleLog.ConsoleLog( $"User wasn't added to DB." );
+                                context.Session ["User"] = "Unknown";
+                                context.Session ["UserID"] = 99999999;
+                            }
+                        }
+                        else
+                        {
+                            context.Session ["User"] = $"{ad.MemberName} [{ad.MemberID}]";
+                            context.Session ["UserID"] = ad.MemberID;
+                        }
+                        JSConsoleLog.ConsoleLog( $"Logged user {ad.MemberName}, ID:{ad.MemberID}" );
                         filterContext.HttpContext.Response.Redirect(redirectTo, true);
                     }
                 }
